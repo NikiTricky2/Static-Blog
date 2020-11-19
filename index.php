be5d73ec9edb9c50fb17d86989cbdc8ccad5d495
NikiTricky2 <63540250+NikiTricky2@users.noreply.github.com>
2020-11-19 20:19:31 +0200

    Not Done Yet

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
     <h1>Hello! My name is Nikola!</h1>
     <p>Nice to meet you! I'm a hobby full-stack programmer, living in Bulgaria. I decided to make a small website with a blog and latest projects.</p>
 
+    <?php
+        echo "Hello";
+    ?>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
